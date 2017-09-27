3403db2e4c662e0f2146139c4b5358b38dd874f1
DLDLD <31480436+DLDLD@users.noreply.github.com>
2017-09-26 23:58:07 -0400

    Implemented remove function

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -1,105 +1,165 @@
-angular.module('listings').controller('ListingsController', ['$scope', '$location', '$stateParams', '$state', 'Listings', 
-  function($scope, $location, $stateParams, $state, Listings){
-    $scope.find = function() {
-      /* set loader*/
-      $scope.loading = true;
+angular.module('listings').controller('ListingsController', ['$scope', '$location', '$stateParams', '$state', 'Listings',
+		function ($scope, $location, $stateParams, $state, Listings) {
+			$scope.find = function () {
+				/* set loader*/
+				$scope.loading = true;
 
-      /* Get all the listings, then bind it to the scope */
-      Listings.getAll().then(function(response) {
-        $scope.loading = false; //remove loader
-        $scope.listings = response.data;
-      }, function(error) {
-        $scope.loading = false;
-        $scope.error = 'Unable to retrieve listings!\n' + error;
-      });
-    };
+				/* Get all the listings, then bind it to the scope */
+				Listings.getAll().then(function (response) {
+					$scope.loading = false; //remove loader
+					$scope.listings = response.data;
+				}, function (error) {
+					$scope.loading = false;
+					$scope.error = 'Unable to retrieve listings!\n' + error;
+				});
+			};
 
-    $scope.findOne = function() {
-      debugger;
-      $scope.loading = true;
+			$scope.findOne = function () {
+				debugger;
+				$scope.loading = true;
 
-      /*
-        Take a look at 'list-listings.client.view', and find the ui-sref attribute that switches the state to the view 
-        for a single listing. Take note of how the state is switched: 
+				/*
+				Take a look at 'list-listings.client.view', and find the ui-sref attribute that switches the state to the view
+				for a single listing. Take note of how the state is switched:
 
-          ui-sref="listings.view({ listingId: listing._id })"
+				ui-sref="listings.view({ listingId: listing._id })"
 
-        Passing in a parameter to the state allows us to access specific properties in the controller.
+				Passing in a parameter to the state allows us to access specific properties in the controller.
 
-        Now take a look at 'view-listing.client.view'. The view is initialized by calling "findOne()". 
-        $stateParams holds all the parameters passed to the state, so we are able to access the id for the 
-        specific listing we want to find in order to display it to the user. 
-       */
+				Now take a look at 'view-listing.client.view'. The view is initialized by calling "findOne()".
+				$stateParams holds all the parameters passed to the state, so we are able to access the id for the
+				specific listing we want to find in order to display it to the user.
+				 */
 
-      var id = $stateParams.listingId;
+				var id = $stateParams.listingId;
 
-      Listings.read(id)
-              .then(function(response) {
-                $scope.listing = response.data;
-                $scope.loading = false;
-              }, function(error) {  
-                $scope.error = 'Unable to retrieve listing with id "' + id + '"\n' + error;
-                $scope.loading = false;
-              });
-    };  
+				Listings.read(id)
+				.then(function (response) {
+					$scope.listing = response.data;
+					$scope.loading = false;
+				}, function (error) {
+					$scope.error = 'Unable to retrieve listing with id "' + id + '"\n' + error;
+					$scope.loading = false;
+				});
+			};
 
-    $scope.create = function(isValid) {
-      $scope.error = null;
+			$scope.create = function (isValid) {
+				$scope.error = null;
 
-      /* 
-        Check that the form is valid. (https://github.com/paulyoder/angular-bootstrap-show-errors)
-       */
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'articleForm');
+				/*
+				Check that the form is valid. (https://github.com/paulyoder/angular-bootstrap-show-errors)
+				 */
+				if (!isValid) {
+					$scope.$broadcast('show-errors-check-validity', 'articleForm');
 
-        return false;
-      }
+					return false;
+				}
 
-      /* Create the listing object */
-      var listing = {
-        name: $scope.name, 
-        code: $scope.code, 
-        address: $scope.address
-      };
+				/* Create the listing object */
+				var listing = {
+					name: $scope.name,
+					code: $scope.code,
+					address: $scope.address
+				};
 
-      /* Save the article using the Listings factory */
-      Listings.create(listing)
-              .then(function(response) {
-                //if the object is successfully saved redirect back to the list page
-                $state.go('listings.list', { successMessage: 'Listing succesfully created!' });
-              }, function(error) {
-                //otherwise display the error
-                $scope.error = 'Unable to save listing!\n' + error;
-              });
-    };
+				/* Save the article using the Listings factory */
+				Listings.create(listing)
+				.then(function (response) {
+					//if the object is successfully saved redirect back to the list page
+					$state.go('listings.list', {
+						successMessage: 'Listing succesfully created!'
+					});
+				}, function (error) {
+					//otherwise display the error
+					$scope.error = 'Unable to save listing!\n' + error;
+				});
+			};
 
-    $scope.update = function(isValid) {
-      /*
-        Fill in this function that should update a listing if the form is valid. Once the update has 
-        successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error 
-        occurs, pass it to $scope.error. 
-       */
-    };
+			$scope.update = function (isValid) {
+				/*
+				Fill in this function that should update a listing if the form is valid. Once the update has
+				successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error
+				occurs, pass it to $scope.error.
+				 */
+				$scope.error = null;
 
-    $scope.remove = function() {
-      /*
-        Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise, 
-        display the error. 
-       */
-    };
+				//Check that the form is valid
+				if (!isValid) {
+					$scope.broadcast('show-errors-check-validity', 'articleForm');
+					return false;
+				}
 
-    /* Bind the success message to the scope if it exists as part of the current state */
-    if($stateParams.successMessage) {
-      $scope.success = $stateParams.successMessage;
-    }
+				var id = $stateParams.listingId;
 
-    /* Map properties */
-    $scope.map = {
-      center: {
-        latitude: 29.65163059999999,
-        longitude: -82.3410518
-      }, 
-      zoom: 14
-    }
-  }
-]);
+				//Create listing object
+				var listing = {
+					name: $scope.name,
+					code: $scope.code,
+					address: $scope.address
+				};
+
+				$scope.loading = true;
+
+				//Update using listings factory
+				Listings.update(id, listing)
+				.then(function (response) {
+					//if successful redirect to list page
+					$state.go('listings.list', {
+						successMessage: 'Listing succesfully updated!'
+					});
+					$scope.loading = false;
+					//otherwise display error
+				}, function (error) {
+					$scope.error = 'Unable to update listing!\n' + error;
+					$scope.loading = false;
+				});
+
+			};
+
+			$scope.remove = function () {
+				/*
+				Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise,
+				display the error.
+				 */
+				$scope.error = null;
+
+				var id = $stateParams.listingId;
+
+				Listings.delete (id)
+				.then(function (response) {
+					//if successful redirect to list page
+					$state.go('listings.list', {
+						successMessage: 'Listing succesfully deleted!'
+					});
+					$scope.loading = false;
+					//otherwise display error
+				}, function (error) {
+					$scope.error = 'Unable to delete listing!\n' + error;
+					$scope.loading = false;
+				});
+			};
+
+			/* Bind the success message to the scope if it exists as part of the current state */
+			if ($stateParams.successMessage) {
+				$scope.success = $stateParams.successMessage;
+			}
+
+			/* Map properties */
+			$scope.map = {
+				center: {
+					latitude: 29.65163059999999,
+					longitude: -82.3410518
+				},
+				zoom: 14
+			}
+
+			$scope.windowOptions = {
+				show: false
+			};
+
+			//Toggles the map's marker window
+			$scope.toggleMarkerWindow = function () {
+				$scope.windowOptions.visible = !$scope.windowOptions.visible;
+			};
+		}
+	]);
